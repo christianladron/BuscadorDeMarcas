ce4018e773e96ea744eace2d629f5fe3ab836e87
Christian Ladrón <christianlgr@gmail.com>
2015-02-21 00:06:36 -0600

    sin nombre con caracter raro en PDFs de resultados

--- marcas.py
+++ marcas.py
@@ -8,7 +8,7 @@ for nombre in lista:
     argPDFfinal = r.text[argPDFinicio:].find("/")
     argPDF = r.text[argPDFinicio:argPDFinicio+argPDFfinal]
     p = requests.get(url="http://marcanet.impi.gob.mx/marcanet/controler/FoneticaPDF/_denominacion/"+argPDF+"/_mc/direct")
-    PDF = open("resultados/"+nombre+".pdf","w")
+    PDF = open("resultados/"+nombre[:-1]+".pdf","w")
     PDF.write(p.content)
     PDF.close()
 lista.close()
