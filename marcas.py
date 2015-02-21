597bcba2fffb5b5788a4b17a9206cada9a996706
Christian Ladrón <christianlgr@gmail.com>
2015-02-21 00:00:01 -0600

    primer commit que funciona

---
+++ marcas.py
@@ -0,0 +1,14 @@
+import requests
+
+lista = open("lista.csv")
+for nombre in lista:
+    print nombre
+    r=requests.post(url="http://marcanet.impi.gob.mx/marcanet/controler/FoneticaLista",headers={"Content-Type":"application/x-www-form-urlencoded"},data="denominacion="+nombre+"&clase=32")
+    argPDFinicio = r.text.find("PDF")+18
+    argPDFfinal = r.text[argPDFinicio:].find("/")
+    argPDF = r.text[argPDFinicio:argPDFinicio+argPDFfinal]
+    p = requests.get(url="http://marcanet.impi.gob.mx/marcanet/controler/FoneticaPDF/_denominacion/"+argPDF+"/_mc/direct")
+    PDF = open(nombre+".pdf","w")
+    PDF.write(p.content)
+    PDF.close()
+lista.close()
